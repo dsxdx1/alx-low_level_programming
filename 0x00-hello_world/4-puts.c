7fea104dbaaedbe5933c5e18c67df4a4be40bcee
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-12 12:55:28 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - this is the main function
+ * description - prints a put word
+ * return - the return prints a zero
+ */
 int main(void)
 {
 	char word;
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints using printf
+ * Return: returns zero 
+*/
+int main(void);
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
