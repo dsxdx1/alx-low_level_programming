27ed3f942ac0f15f6f1308e8ea05db1940ff90ab
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-12 07:27:07 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	char word[] = "Programming is like building a multilingual puzzle";
+	
+	puts(word);
+	
 	return(0);
 }
