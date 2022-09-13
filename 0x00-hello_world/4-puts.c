8c57ee97a7b2fe0660470a2f0922566f41393b84
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-12 22:06:51 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 /**
  * main - this is the main function
  * description - prints a put word
- * return: the return prints a zero
+ * Return: the return prints a zero
  */
 int main(void)
 {
 	char word[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(word);
 	return (0);
 }
