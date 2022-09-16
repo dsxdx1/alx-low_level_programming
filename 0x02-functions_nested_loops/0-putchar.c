ae7cc47b8ebc5e9dd742d4e5d903b8e0001f3768
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-16 08:41:29 -0700

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints _putchar to stdout
+ *
+ * Return: returns 0.
+ */
+
+int main(void)
+{
+	char *i	= "_putchar";
+		
+	while(*i)
+	{
+		_putchar(*i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * main - check code 
+ *
+ * Return: Always 0.
+ */
+
+
