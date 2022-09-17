fc5dc184a530a6276085c74e786ac122290b2c62
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-17 02:52:53 -0700

    updated

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign -  prints the sign of a numnber
+ * @n: The argument is an int
+ * Return: returns 1 if +ve, 0 if 0 and -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
