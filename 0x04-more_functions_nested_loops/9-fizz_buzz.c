adad20b583ada2f224099b5c14c2af69cb3b7e73
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-18 14:28:32 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - iterates from 1-100 
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "Fizz Buzz";
+	char i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("%s", f);
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("%s", b);
+		}
+		else if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("%s", fb);
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	
+}
