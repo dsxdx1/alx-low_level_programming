e8af248c1c51b29558909d9d2b7d6ae60b749cfa
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-18 03:29:18 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line 
+ * @n: number of lines
+ * Return: returns void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
