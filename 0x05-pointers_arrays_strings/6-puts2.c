7b8d4d3791166cafba05dc6d2f693cbc09a8eea8
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-28 16:10:09 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,9 @@
  */
 void puts2(char *str)
 {
-	int i;
-	int len = 0;
+	int i = 0;
 
-	while (str[len] != '\0')
-		len++;
-	for (i = 0 ; i <= len ; i++)
+	for (; str[i] != '\0' ; i++)
 	{
 		if (i % 2 == 0)
 		{
