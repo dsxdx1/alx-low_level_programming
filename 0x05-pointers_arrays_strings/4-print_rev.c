d933f7228ceff80bb4eb3727554fc963b4da085f
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-28 16:20:50 -0700

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,12 @@ void print_rev(char *s)
 {
 	int len = 0;
 	int i;
-	char *t = s;
 
-	while (*t != '\0')
+	while (s[len] != '\0')
 	{
-		t++;
 		len++;
 	}
-	for (i = len ; i > 0 ; --i)
+	for (i = len - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
