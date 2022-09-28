dad33d457684bb44133537c768cefe5f911ac045
dsxdx1 <oduimmanuel21@gmail.com>
2022-09-28 16:40:36 -0700

    updated

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints the odd numbers
+ * @s: the pointer array
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+	int n;
+	
+	while (str[len] != '\0')
+		len++;
+	if ((len % 2) == 0)
+		n = (len / 2);
+	else
+		n = ((len - 1) / 2);
+	for (i = n ; i <= (len - 1) ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
